c6b224046ca1fd4608f0e312bdb7d60ddac6a20c
TavrsD <dtavares@id.uff.br>
2025-02-17 21:24:59 -0300

    wip: patch para dados de adocao

--- scripts/adotarLobo.js
+++ scripts/adotarLobo.js
@@ -1,35 +1,39 @@
-const { json } = require("body-parser");
-
-const baseURL = 'http://localhost:3000/lobos';
-const adotarBtn = document.getElementById('botao_adota');
+const baseURL = "http://localhost:3000";
 
 function getIdFromURL() {
-    const params = new URLSearchParams(window.location.search);
-    return params.get('id');
+  const params = new URLSearchParams(window.location.search);
+  return params.get("id");
 }
+const adotarBtn = document.getElementById("botao_adota");
 
-function adotarLobinho() {
-    const id = getIdFromURL();
-    const formularioAdocao = {
-        adotado: true,
-        nomeDono: document.getElementById('nome'),
-        idadeDono:document.getElementById('idade'),
-        emailDono:document.getElementById('email'),
-    };
-    fetch(`${baseURL}/${id}`, {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formularioAdocao)
-    })
-    .then(response => response.json())
+function adotarLobinho(event) {
+    event.preventDefault();
+
+  const id = getIdFromURL();
+  if (!id) {
+    alert("ID do lobinho não encontrado na URL.");
+    return;
+  }
+
+  const formularioAdocao = {
+    adotado: true,
+    nomeDono: document.getElementById("nome").value,
+    idadeDono: parseInt(document.getElementById("idade").value),
+    emailDono: document.getElementById("email").value,
+  };
+  fetch(`${baseURL}/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formularioAdocao),
+  })
+    .then((response) => response.json())
     .then(() => {
-        alert('Adotado');
-        window.location.href = './html/home.html';
+      alert("Adotado");
+      window.location.href = "home.html";
     })
-    .catch(error => console.error('Erro ao adotar o lobinho:', error));
+    .catch((error) => console.error("Erro ao adotar o lobinho:", error));
 }
 
-adotarBtn.addEventListener('click', console.log('clicou')
-);
+adotarBtn.addEventListener("click", adotarLobinho);
